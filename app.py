51ac1f817c7bb971ad9b6308e3c00fa792f63d25
Brandon Zemel <zemelb@gmail.com>
2020-08-06 16:33:52 -0400

    implemented show_venue

--- app.py
+++ app.py
@@ -179,7 +179,7 @@ def search_venues():
 @app.route('/venues/<int:venue_id>')
 def show_venue(venue_id):
   # shows the venue page with the given venue_id
-  # TODO: replace with real venue data from the venues table, using venue_id
+  # DONE: replace with real venue data from the venues table, using venue_id
   # NOT WORKING YET
   
   # query db for the venue's ID
@@ -187,7 +187,7 @@ def show_venue(venue_id):
 
   # if it finds a venue with that ID
   if venuequery:
-    venue_details = Venue
+    venue_details = venuequery
 
     data = {
       "id": venue_details.id, 
@@ -203,33 +203,9 @@ def show_venue(venue_id):
       "seeking_description": venue_details.seeking_description, 
       "image_link": venue_details.image_link, 
       }
-  
-  # data1={
-  #   "id": 1,
-  #   "name": "The Musical Hop",
-  #   "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-  #   "address": "1015 Folsom Street",
-  #   "city": "San Francisco",
-  #   "state": "CA",
-  #   "phone": "123-123-1234",
-  #   "website": "https://www.themusicalhop.com",
-  #   "facebook_link": "https://www.facebook.com/TheMusicalHop",
-  #   "seeking_talent": True,
-  #   "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-  #   "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-  #   "past_shows": [{
-  #     "artist_id": 4,
-  #     "artist_name": "Guns N Petals",
-  #     "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-  #     "start_time": "2019-05-21T21:30:00.000Z"
-  #   }],
-  #   "upcoming_shows": [],
-  #   "past_shows_count": 1,
-  #   "upcoming_shows_count": 0,
-  # }
-  
-  # data = list(filter(lambda d: d['id'] == venue_id, [data1, data2, data3]))[0]
+ 
   return render_template('pages/show_venue.html', venue=data)
+  
 
 #  Create Venue
 #  ----------------------------------------------------------------
